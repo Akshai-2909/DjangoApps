057f145a92bab402555b49446d1c384e055af933
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2021-11-21 13:23:31 +0530

    Update log.py

--- src/log.py
+++ src/log.py
@@ -1,3 +1,12 @@
 import datetime, time
+
+path = "C:/Users/Desktop/Cn-project/main/log/log.txt"
+
 def getTimeStamp():
     return "[" + str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')) + "]"
+
+
+def write(msg):
+    with open(path, "a+") as file:
+        file.write(msg)
+        file.write("\n")
